f6da08e5c0bc240ef6bad13dd788a056ee327484
praveen19bits <praveen19bits@gmail.com>
2023-05-20 17:15:05 +0530

    Organize Imports

--- src/main/java/com/polarbookshop/catalogservice/HomeController.java
+++ src/main/java/com/polarbookshop/catalogservice/HomeController.java
@@ -1,8 +1,6 @@
 package com.polarbookshop.catalogservice;
 
 import com.polarbookshop.catalogservice.config.PolarProperties;
-import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
